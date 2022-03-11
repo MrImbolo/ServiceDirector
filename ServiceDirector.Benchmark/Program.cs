31a4562efb1c4eb53d1e403818b88ff49222dc62
MrImbolo <myself0094@gmail.com>
2022-03-11 19:12:54 +0300

    Added benchmark

---
+++ ServiceDirector.Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace ServiceDirector.Benchmark;
+
+public class Program
+{
+    static void Main()
+    {
+        _ = BenchmarkRunner.Run<ServiceDirectorBenchmarks>();
+    }
+}
---
+++ ServiceDirector.Benchmark/ServiceDirectorBenchmarks.cs
@@ -0,0 +1,40 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceDirector.Benchmark
+{
+    [MemoryDiagnoser]
+    [RankColumn]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
+    public class ServiceDirectorBenchmarks
+    {
+        [Benchmark]
+        public static async Task ServiceDirectorExecuteAsync()
+        {
+            var service = new DefaultService();
+            var director = new DefaultServiceDirector();
+
+            ServiceRequest request2 = new() { Data = "This a sample director request data" };
+
+            _ = await director.ExecuteAsync(service, async (ct) =>
+            {
+                return await Task.FromResult(request2);
+            });
+        }
+
+
+        [Benchmark]
+        public static async Task PlainServiceExecuteAsync()
+        {
+            var service = new DefaultService();
+            ServiceRequest request1 = new() { Data = "This is a sample data" };
+
+            _ = await service.ExecuteAsync(request1);
+        }
+    }
+}
